6311040654761a6e784f20bf1dcecb7a2d9cc8e0
eleksbai <eleksbai@qq.com>
2025-03-28 14:19:15 +0800

    chap2: new application

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Estate",
+    'summary': """
+        Estate demo summary
+    """,
+    'description': """
+        Estate demo description
+    """,
+    'version': '17431.13.23',
+    'application': True,
+    'installable': True,
+    'author': "Eleksbai",
+    'depends': ['base'],
+    'license': 'AGPL-3'
+}
