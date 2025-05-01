bf839cc9a6bb6be22172aecb762c40d68a75950e
eleksbai <eleksbai@qq.com>
2025-05-01 17:24:23 +0800

    fix: The id name is the same as the module name

--- ee_estate/__manifest__.py
+++ ee_estate/__manifest__.py
@@ -24,9 +24,10 @@
         'views/estate_property_tags.xml',
         'views/estate_property_views.xml',
         'views/property_type.xml',
-        'views/estate_menus.xml',
+
         'views/estate_salesperson_property.xml',
         'views/estate_kanban.xml',
+        'views/estate_menus.xml',
 
         # 'views/real_estate_property_type_views.xml',
         # 'views/real_estate_property_views.xml',  # Depends on `real_estate_offer_views.xml`.
