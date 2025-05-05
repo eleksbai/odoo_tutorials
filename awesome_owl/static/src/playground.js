9febb585a6d064c3e3d362bbf5959a0c45a56a27
eleksbai <eleksbai@qq.com>
2025-05-05 18:26:07 +0800

    Chapter 1: Owl components(2. Extract in a sub component)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,16 +1,11 @@
 /** @odoo-module **/
 
 import {Component, useState} from "@odoo/owl";
+import {Counter} from "./counter/counter"; // 假设 Counter 组件在 counter.js 文件中
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = {Counter}; // 注册 Counter 子组件
 
-    setup() {
-        this.state = useState({value: 0});
-    }
-
-    increment() {
-        this.state.value++;
-    }
 }
 
