1c4d0e18ec0f05584215f716b232cf09316c4346
eleksbai <eleksbai@qq.com>
2025-05-08 16:37:11 +0800

    Chapter 1: Owl components(3. A simple componentCard)

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,10 +2,11 @@
 
 import {Component, useState} from "@odoo/owl";
 import {Counter} from "./counter/counter"; // 假设 Counter 组件在 counter.js 文件中
+import {Card} from "./card/card"; //
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = {Counter}; // 注册 Counter 子组件
+    static components = {Counter, Card}; // 注册 Counter 子组件
 
 }
 
