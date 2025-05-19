2db45aa2a25cca386465f46c354ee25772b68e5c
eleksbai <eleksbai@qq.com>
2025-05-19 17:56:29 +0800

    Chapter 2: Build a dashboard(2. Add some buttons for quick navigation)

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,12 +1,27 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { registry } from "@web/core/registry";
+import {Component} from "@odoo/owl";
+import {registry} from "@web/core/registry";
 import {Layout} from "@web/search/layout"
+import {useService} from "@web/core/utils/hooks";
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = {Layout};
+
+    setup() {
+        this.action = useService("action");
+    }
+
+    openCustomers() {
+        this.action.doAction("base.action_partner_form");
+    }
+    openLead() {
+        this.action.doAction("crm.crm_lead_all_leads");
+        // this.action.doAction("crm.crm_lead_action_my_activities_view_tree");
+    }
+
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
