c264445469414c414ff7076470264a6b463f0c28
eleksbai <eleksbai@qq.com>
2025-05-19 13:47:16 +0800

    Chapter 1: Owl components(14. Minimizing card content)

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -5,15 +5,24 @@ import {Component, useState} from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card";
     static description = "Card component";
+
+    setup() {
+        this.state = useState({isOpen: true});
+    }
+
     // static props = ['title?' ];
     // static props = {
     //     'title': {type: String, validate: s => s.length > 5 && s.length < 10},
     //     'size': {
     //         type: Number, validate: n => n > 10
     //     },
-        // 'content': {type: String},
-        // 'note': {type: 'string', optional: true}
+    // 'content': {type: String},
+    // 'note': {type: 'string', optional: true}
     // };
+    toggle() {
+        debugger
+        this.state.isOpen = !this.state.isOpen;
 
+    }
 }
 
